74c6fd7b9f9c2069c0d7d392a163e10907c6a05e
dyh的gitee <2021173146@qq.com>
2025-05-22 20:34:40 +0800

    修改组件

--- chat-bot/app/auth/callback/page.tsx
+++ chat-bot/app/auth/callback/page.tsx
@@ -1,28 +1,11 @@
 'use client'
-import { useEffect } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Suspense } from 'react'
+import AuthCallback from '../_AuthCallback'
 
-export default function AuthCallback() {
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  const code = searchParams.get('code')
-  const next = searchParams.get('next') ?? '/'
-
-  useEffect(() => {
-    const supabase = createClientComponentClient()
-    if (code) {
-      supabase.auth.exchangeCodeForSession(code).then(({ error }) => {
-        if (!error) {
-          router.replace(next)
-        } else {
-          router.replace('/auth/auth-code-error')
-        }
-      })
-    } else {
-      router.replace('/auth/auth-code-error')
-    }
-  }, [code, next, router])
-
-  return <div>正在登录中...</div>
+export default function Page() {
+  return (
+    <Suspense fallback={<div>正在登录中...</div>}>
+      <AuthCallback />
+    </Suspense>
+  )
 } 
